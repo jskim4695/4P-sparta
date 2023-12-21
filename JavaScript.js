a1a7fabf71fd5ae9af79c8510eef4c494127bb04
KimJaeYeon~ <popcon940620@gmail.com>
2023-12-21 21:06:12 +0900

    Button

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,14 @@
-console.log(`hello world!`);
+
+// 클래스명 변수 선언
+const button = $('.button'); 
+const tabContainer = $('.tab-container');
+const tabDesc = $('.tab-desc')
+
+//.tab-container 자식요소의 길이만큼 
+for (let i = 0; i < tabContainer.children().length; i++) {
+  button.eq(i).on('click', function (){
+  tabDesc.removeClass('show');
+  tabDesc.eq(i).addClass('show');
+
+})
+};
